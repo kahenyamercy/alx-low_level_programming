cef579c6aa0d568c44b55d03d557df890cf84194
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-17 12:51:30 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-	/*declare a variable*/
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		/*print the value of the variable*/
 		putchar(alphabet);
 	}
 
