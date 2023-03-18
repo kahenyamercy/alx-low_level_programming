2f599bd1586999604047b03a59be3ed70a89679b
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-18 20:21:27 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:Prints all digit number of
+ * base 10 starting from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
