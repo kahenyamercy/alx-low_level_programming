3d510fc62329996f81dabc762988c7e65b345336
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-22 10:15:36 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1, b = 2, c;
+	int i;
+
+	printf("%d, %d, ", a, b);
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		printf("%d", c);
+		if (i < 98)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
