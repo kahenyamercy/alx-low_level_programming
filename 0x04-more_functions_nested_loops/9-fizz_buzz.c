47999c59b196686cce74d73b0acc5f8eead62bec
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-23 17:07:35 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Fizz-Buzz test
+ * Description: prints nmbers from 1 to 100,replacing multiples
+ * of 3 with "Fizz" and multiples of 5 with "Buzz" and multiples
+ * of both with "FizzBuzz"
  * Return: 0
  */
 int main(void)
