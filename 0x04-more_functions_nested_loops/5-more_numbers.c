ce13cde4055c6b521d83490a795d2079b98fb05c
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-23 12:30:05 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,12 +4,11 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
 			{
@@ -17,6 +16,7 @@ void more_numbers(void)
 			}
 			_putchar(j % 10 + '0');
 		}
+
 		_putchar('\n');
 	}
 }
