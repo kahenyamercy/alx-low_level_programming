442a2820c781a852134e7a9e706f370a32efb6ba
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-26 10:56:03 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,19 +3,23 @@
  * main - entry point
  * Return: 0
  */
-
 int main(void)
 {
-	unsigned long a = 1, b = 2, c, count = 3;
+	unsigned long a = 1, b = 2;
+	unsigned long i;
 
-	printf("%lu, %lu", a, b);
-	while (count < 99)
+	printf("%lu, %lu ", a, b);
+	for (i = 2; i < 98; i++)
 	{
-		c = a + b;
+		unsigned long c = a + b;
+
+		printf("%lu", c);
+		if (i < 97)
+		{
+			printf(", ");
+		}
 		a = b;
 		b = c;
-		printf(", %lu", c);
-		count++;
 	}
 	printf("\n");
 	return (0);
