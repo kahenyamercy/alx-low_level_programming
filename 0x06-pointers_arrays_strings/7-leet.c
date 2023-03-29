979656b4bf5370916c48f2a6a9e71bc56a1b8e2e
kahenyamercy <kahenyamercy5@gmail.com>
2023-03-29 21:16:46 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *leet - encodes a string into 1337
+ * @s:  modified string
+ * Return: s
+ */
+char *leet(char *s)
+{
+	char *letters = "AEOTLaeotl";
+	char *numbers = "4307104310";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
