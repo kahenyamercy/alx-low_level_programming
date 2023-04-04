6f74ae9cfd26739b300d38899c1bc93b94ca6c5d
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-04 12:22:28 +0000

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set a value of a pointe to a char
+ * @s: double pointer
+ * @to: pointer
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
