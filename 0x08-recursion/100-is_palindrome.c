0ca945856566b5c0d153a0eef6517f9846a169dd
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-07 07:29:14 +0000

    palindromes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,23 +3,41 @@
 /**
  * is_palindrome - returns a palindrome
  * @s: string to be checked
+ * @left: the left index in the string
+ * @right: the right index in the string
+ * Return: 1 if string is a palindrome else 0
+ */
+int is_palindrome_helper(char *s, int left, int right)
+{
+	if (left >= right)
+	{
+		return (1);
+	}
+	else if (s[left] != s[right])
+	{
+		return (0);
+	}
+	else 
+	{
+		return (is_palindrome_helper(s, left + 1, right - 1));
+	}
+}
+
+/**
+ * is_palindrome - returns a palindrome
+ * @s: string to be checked
  * Return: 1 if string is a palindrome else 0
  */
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
-	int m = 0;
-	int t = 0;
+	int length = strlen(s);
+	int left = 0;
+	int right = length -1;
 
-	if (len == 0 || len == 1)
-	{
-		return (1);
-	}
-	if (m == t)
-	{
-		return (1);
-	}
-	m++;
-	t--;
-	return (0);
+		if (length == 0 || length == 1)
+		{
+			return (1);
+		}
+	return (is_palindrome_helper(s, left, right));
+
 }
