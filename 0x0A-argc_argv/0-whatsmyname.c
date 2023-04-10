b78d747428bfd08179befce8845298358ca32c1c
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-10 09:34:55 +0000

    write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * @argc:argument count
+ * @argv:argument vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void) argc;
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+ARGC "argument count" - contains number of arguments passed to the program
+ARGV "argument vector" - 1D array of string
