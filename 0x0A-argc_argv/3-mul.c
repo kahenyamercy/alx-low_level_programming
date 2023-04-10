f4f93fb513d2f0c185400b1d4c72ac6e95c6ed3c
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-10 14:23:07 +0000

    multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc:number of arguments
+ * @argv:argument vector
+ * Return: 0 success 1 error
+ */
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (a == 0 || b == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = a * b;
+		printf("%d\n", result);
+		return (0);
+	}
+}
