5d8e2014229774e213842f1ca91274fa77703f14
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-11 21:31:12 +0000

    changes made to concatenated function 2nd time

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,8 @@ char *str_concat(char *s1, char *s2)
 	s3 = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 		return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return ("");
 	for (j = 0; j < len1; j++)
 		s3[j] = s1[j];
 	for (i = 0; i < len2; i++)
