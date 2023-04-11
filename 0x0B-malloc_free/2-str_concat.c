5bbd5da928cbd6a94ffdbd93d2f12071bd4d0f11
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-11 22:23:52 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,11 +18,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	s3 = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 		return (NULL);
-	memcpy(s3, s1, len1);
-	memcpy(s3 + len1, s2, len2);
-	s3[len1 + len2] = '\0';
+	strncpy(s3, s1, len1);
+	strncpy(s3 + len1, s2, len2 + 1);
 	return (s3);
 }
