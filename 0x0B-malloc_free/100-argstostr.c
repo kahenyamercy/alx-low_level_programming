a04816f7e2ccf98c05cf5fd62a3c7d0528291ff6
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-12 10:52:52 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,14 +21,14 @@ char *argstostr(int ac, char **av)
 			return (NULL);
 		len += strlen(av[i]) + 1;
 	}
-	newstr = malloc(len * sizeof(char));
+	newstr = malloc((len + 1) * sizeof(char));
 	if (newstr == NULL)
 		return (NULL);
-	for (i =0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			newstr[position++] = av[i][j];
-		newstr [position++] = '\n';
+		newstr[position++] = '\n';
 	}
 	newstr[position] = '\0';
 	return (newstr);
