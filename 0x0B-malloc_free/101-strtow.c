093f9d45686c848304d3983d847474f4d025090e
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-12 15:07:40 +0000

    splitting a string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * strtow - splits a string to words
+ * @str: the string to be split
+ * Return:pointer to array of strings
+ */
+char **strtow(char *str)
+{
+	char **nwords;
+	char *word;
+	int n = strlen(str);
+	int i = 0;
+	int num_words = 0;
+	int len = 0;
+	int start = i;
+	int j;
+
+	if (str == NULL || strlen(str) == 0)
+		return (NULL);
+	nwords = calloc(n, sizeof(char *));
+	if (nwords == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		if (isspace(str[i]) || str[i] == '\0')
+		{
+			len = i - start;
+			if (len > 0)
+			{
+				word = malloc((len + 1) * sizeof(char));
+				if (word == NULL)
+				{
+					for (j = 0; j < num_words; j++)
+						free(nwords[j]);
+					free(nwords);
+					return (NULL);
+				}
+				memcpy(word, str + start, len);
+				word[len] = '\0';
+				nwords[num_words++] = word;
+			}
+			start = i + 1;
+		}
+		i++;
+	}
+	nwords[num_words] = NULL;
+	return (nwords);
+}
