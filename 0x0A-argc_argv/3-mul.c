8f34a3c207ba100daccdd7d9be8155b8f1536e4a
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-12 16:05:39 +0000

    fixed a segmentation fault by initializing variables after checking the no of arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
  * main - entry point
  * @argc:number of arguments
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a;
+	int b;
 	int result;
 
 	if (argc != 3)
@@ -17,6 +17,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	result = a * b;
 	printf("%d\n", result);
 
