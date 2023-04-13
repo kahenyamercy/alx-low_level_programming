48a0f8e9bcd64497b03809cd591eca0215483872
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-13 15:46:50 +0000

    added a condition to check if the strings are empty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0;
 
 	if (s1 == NULL || s2 == NULL)
-		return ("");
+		return (NULL);
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
