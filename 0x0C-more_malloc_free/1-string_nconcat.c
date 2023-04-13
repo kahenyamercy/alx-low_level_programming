c5509a67b2372d9f9ac2dd05717c2abf0a42e9cb
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-13 16:02:27 +0000

    concatinating string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		s3[i] = s1[i];
-	for (j = 0; j < n || j < len2; j++)
+	for (j = 0; j < n && j < len2; j++)
 		s3[i + j] = s2[j];
 	s3[i + j] = '\0';
 	return (s3);
