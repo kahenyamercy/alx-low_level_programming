4ff64298b5f81e9cebf709cecf5624b52158a2f5
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-13 15:03:43 +0000

    concatenate strings with a specific number of bytes

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2:second string
+ * @n: number of s2 bytes to be concatenated with s1
+ * Return: pointer to a new space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+	unsigned int i, j;
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+
+	if (len1 == 0 || len2 == 0)
+		return ("");
+	s3 = malloc(sizeof(char) * (len1 + n + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s3[i] = s1[i];
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+		s3[i + j] = s2[j];
+	s3[i + j] = '\0';
+	return (s3);
+
+}
