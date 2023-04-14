3eff78ca304b1057cced39bbdefe1a46dff41b9b
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-14 07:57:22 +0000

    sets memory to zero

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,18 +9,22 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newpointer;
-	/*unsigned int i;*/
+	void *newpointer = malloc(new_size);
+	unsigned int i;
+	unsigned char *p = ptr;
+	unsigned char *np = newpointer;
 
 	if (ptr == NULL)
 		return (malloc(new_size));
 	if (new_size == old_size)
 		return (ptr);
-	newpointer = malloc(new_size);
 	if (newpointer == NULL)
 		return (NULL);
 	if (new_size < old_size)
 		old_size = new_size;
+	for (i = 0; i < old_size && i < new_size; i++)
+		np[i] = p[i];
+
 	free(ptr);
 	return (newpointer);
 }
