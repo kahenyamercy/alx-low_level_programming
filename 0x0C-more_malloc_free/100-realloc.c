e0aff1dc22d63e1ce5d410c46e18a71b6e7b3bf1
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-14 09:09:02 +0000

    changes in the for loop

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated with a call to malloc
@@ -9,25 +10,31 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newpointer = malloc(new_size);
-	unsigned char *p = ptr;
-	unsigned char *np = newpointer;
-	unsigned int i;
+	void *new_ptr;
 
-	if (newpointer == NULL)
-		return (NULL);
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-		return (malloc(new_size));
-	if (new_size == old_size)
-		return (ptr);
-	for (i = 0; i < old_size && i < new_size; i++)
-		np[i] = p[i];
 
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memcpy(new_ptr, ptr, old_size);
 	free(ptr);
-	return (newpointer);
+	return (new_ptr);
 }
