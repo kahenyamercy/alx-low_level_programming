18c7b94deaaa712d5471d6f853745bbfcdf9d97b
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-16 16:13:11 +0000

    prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,35 +3,21 @@
  * main - Entry point
  * Return: 0
  */
-
 int main(void)
 {
-	int a = 1, b = 2, c, count;
+	unsigned int a = 1, b = 2, c, count;
 
-	putchar(a + '0');
-	putchar(',');
-	putchar(' ');
-	putchar(b + '0');
+	printf("%u, %u", a, b);
 
 	for (count = 3; count <= 98; count++)
 	{
 		c = a + b;
-		putchar(',');
-		putchar(' ');
-		if (c > 9)
-		{
-			putchar((c / 10) + '0');
-			putchar((c % 10) + '0');
-		}
-		else
-		{
-			putchar(c + '0');
-		}
 		a = b;
 		b = c;
+		printf(", %u", c);
 	}
 
-	putchar('\n');
+	printf("\n");
 
 	return (0);
 }
