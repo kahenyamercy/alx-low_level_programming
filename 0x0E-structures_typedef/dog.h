b4d96d8a4e8d2661119eae52a329059f1f7ea111
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-17 09:04:29 +0000

    documentation to the struct that represents information about a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct representing information about a dog
+ * @name: A pointer to the dog's name
+ * @age: dog's age
+ * @owner: A pointer to owner
+ */
 struct dog
 {
-	char *name; 
-	float age; 
+	char *name;
+	float age;
 	char *owner;
 };
 #endif
