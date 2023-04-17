22da41f34be0f7b9bdf0b0ad92dd24462bdb7bb7
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-17 09:38:50 +0000

    added the return void

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,9 +2,10 @@
 /**
  * init_dog - a function that initialize a variable of type struct dog
  * @d: pointer to variable of type struct dog
- * @name: pointer to name of type character
- * @age: float number of age
- * @owner: pointer to owner of type character
+ * @name: pointer to name of the dog
+ * @age: float number of age of the dog
+ * @owner: pointer to owner of the dog
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
