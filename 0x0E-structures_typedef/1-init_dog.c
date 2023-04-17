221d7eae3d36f9950ad9590a6a2b8cdde76b134a
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-17 09:51:55 +0000

    introduced a condition

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+
 /**
  * init_dog - a function that initialize a variable of type struct dog
  * @d: pointer to variable of type struct dog
@@ -9,6 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
