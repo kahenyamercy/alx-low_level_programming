32bb1bbd1e6edd69e054dd9d1a9f7cfe3472e1ba
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-19 17:38:34 +0000

    main file that accepts argument from user and checks conditions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: result of operation 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	int result;
+	int (*func)(int, int);
+	char c = *argv[2];/*Get the operator character*/
+
+	/*Get a function pointer based on operator provided as the second argument*/
+	func = get_op_func(argv[2]);
+
+	if (argc != 4)/*Check if the function pointer is valid*/
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!func)/*Check if the function pointer is valid*/
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/*Check if division or modulo by zero is attempted*/
+	if ((c == '/' || c == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* Perform the operation*/
+	result = func(num1, num2);
+	/*prints the result*/
+	printf("%d\n", result);
+	return (0);
+}
