010e71a431d5d9e2146f67105be18234a5298880
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-19 19:31:06 +0000

    fixed mixed declaration errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,30 +8,35 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
+	int num1;
+	int num2;
 	int result;
 	int (*func)(int, int);
-	char c = *argv[2];/*Get the operator character*/
-
-	/*Get a function pointer based on operator provided as the second argument*/
-	func = get_op_func(argv[2]);
+	char c;
 
 	if (argc != 4)/*Check if the function pointer is valid*/
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
+	/*converts first and third arguments to integers*/
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	/*Get a function pointer based on operator provided as the second argument*/
+	func = get_op_func(argv[2]);
+
 	if (!func)/*Check if the function pointer is valid*/
 	{
 		printf("Error\n");
-		return (99);
+		exit(99);
 	}
+	c = *argv[2];/*Get the operator character*/
+
 	/*Check if division or modulo by zero is attempted*/
 	if ((c == '/' || c == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		return (100);
+		exit(100);
 	}
 	/* Perform the operation*/
 	result = func(num1, num2);
