1e054d100a887fe310ba49f20a2f3146d932d3a2
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-27 12:23:48 +0000

    print before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * printbeforemain - prints message before main is executed
+ * Description: prints the two lines before main
+ */
+void printbeforemain(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
