13bc52eed7b37516f0b41307030171bb234fcbe4
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-27 12:42:07 +0000

    constructor attribute which executes functions automatically

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * printbeforemain - prints message before main is executed
  * Description: prints the two lines before main
  */
-void printbeforemain(void)
+void __attribute__ ((constructor)) printbeforemain()
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
