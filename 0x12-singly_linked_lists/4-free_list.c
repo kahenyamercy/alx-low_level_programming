25e3e0c8e989d9d7d19e45d2633e7ec173571cf3
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-27 11:55:32 +0000

    free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees the list
+ * @head: head of the link list
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	ptr = head;
+
+	while (ptr != NULL)
+	{
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+}
