beaf017441915841aa40a06b281369bbb86befa5
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-28 12:13:37 +0000

    free the memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to the first node
+ *
+ * Return; none
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+
+	}
+}
