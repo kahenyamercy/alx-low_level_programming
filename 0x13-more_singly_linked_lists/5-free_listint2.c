77a252599316bfe179d612e5a611eff389348d53
kahenyamercy <kahenyamercy5@gmail.com>
2023-04-30 10:16:36 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 
@@ -15,5 +18,4 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
-	head = NULL;
 }
