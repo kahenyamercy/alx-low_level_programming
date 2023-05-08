c9901d53ac39976f3928d41bd8781bf79f3ab543
kahenyamercy <kahenyamercy5@gmail.com>
2023-05-08 12:14:15 +0000

    creating and writing into a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = " ";
-	while (text_content[num_of_letters])
+	while (!text_content[num_of_letters])
 		num_of_letters++;
 	nwrite = write(fd, text_content, num_of_letters);
 
