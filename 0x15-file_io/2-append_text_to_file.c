2a751fb131c8f686393e6be84cf517bb92893181
kahenyamercy <kahenyamercy5@gmail.com>
2023-05-08 13:19:20 +0000

    appends to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,10 +17,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	while (text_content[num_letters])
 		num_letters++;
-	if (text_content == NULL)
-		return (1);
 	nwrite = write(fd, text_content, num_letters);
 
 	if (nwrite == -1)
