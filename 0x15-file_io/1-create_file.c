c5efde5cf688c34ef3ac6884da2354ca5aa85680
kahenyamercy <kahenyamercy5@gmail.com>
2023-05-08 12:41:13 +0000

    modification to the return value on success

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
-	while (!text_content[num_of_letters])
+		text_content = "";
+
+	while (text_content[num_of_letters])
 		num_of_letters++;
+
 	nwrite = write(fd, text_content, num_of_letters);
 
 	if (nwrite == -1)
@@ -29,5 +31,5 @@ int create_file(const char *filename, char *text_content)
 
 	close(fd);
 
-	return (-1);
+	return (1);
 }
