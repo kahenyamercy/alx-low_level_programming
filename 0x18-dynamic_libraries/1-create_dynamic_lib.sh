82d28c3b98d4ed6badbcfb5b1033f9b9825266e4
kahenyamercy <kahenyamercy5@gmail.com>
2023-08-21 11:09:59 +0000

    chore: update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into object files
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
-
-# Create the dynamic library liball.so from the object files
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -o liball.so *.o
